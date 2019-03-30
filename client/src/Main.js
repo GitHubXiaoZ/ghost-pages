445f293bf2764c04c97e4402eb040c4624ab9ec6
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-03-29 22:47:23 -0400

    Add client

---
+++ client/src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+              <ul className="header">
+              </ul>
+              <div className="content">
+              </div>
+            </div>
+        )
+      }
+    }
+//export main page
+export default Main;
