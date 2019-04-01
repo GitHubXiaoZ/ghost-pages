88f40becb35e0f141e91ae526ddf9603099eb455
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-01 16:23:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 /*imports*/
 const express =  require("express")
 const mongoose = require("mongoose")
+const passport = require("passport")
+
+const users = require("./api/users")
 
 const db = require("./config/dbKeys").mongoURI
 
 /*app*/
 const app = express()
+
+/*passport middleware*/
+app.use(passport.initialize())
+/*config*/
+require("./config/passport")(passport)
 
 /*middleware*/
 app.use(
@@ -25,6 +33,7 @@ mongoose
     .then(() => console.log("Connected!"))
     .catch(err => console.log(err))
 
+app.use("/api/users", users)
 
 const port = process.env.PORT || 3001
 
