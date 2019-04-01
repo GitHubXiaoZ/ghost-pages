cc4131cda864d6a74793455ffcbcc0fe031ba0d4
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-03-31 23:51:23 -0400

    Create login.js

---
+++ valid/login.js
@@ -0,0 +1,29 @@
+/*imports*/
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+/*exports*/
+module.exports = validateLoginInput = data => {
+    let errors = {}
+
+    /*sets fields to empty string if left empty */
+    data.email = !isEmpty(data.email) ? data.email : ""
+    data.password = !isEmpty(data.password) ? data.password : ""
+
+    /*validates email field*/
+    if(Validator.isEmpty(data.email)) {
+        errors.email = "Email required!"
+    } else if(!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid!"
+    }
+
+    /*validates password field*/
+    if(Validator.isEmpty(data.password)) {
+        errors.password = "Password required!"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
