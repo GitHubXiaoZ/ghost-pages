3d8d9acd4300c893bf696a02b61a982f7a883e2b
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-01 15:45:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,29 @@
 const express =  require("express")
 const mongoose = require("mongoose")
 
+const db = require("./config/dbKeys").mongoURI
+
 /*app*/
 const app = express()
+
+/*middleware*/
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+)
+
+app.use(express.json)
+
+/*database*/
+mongoose
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("Connected!"))
+    .catch(err => console.log(err))
+
 
 const port = process.env.PORT || 3001
 
