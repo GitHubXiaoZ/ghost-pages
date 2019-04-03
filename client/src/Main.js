d0ce551e0bd34fd911a65441e2323faaff264ae3
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-03 01:27:31 -0400

    Update Main.js

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import Navbar from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing"
 
 class Main extends Component {
     render() {
         return (
+          <Router>
             <div className="main">
-              <ul className="header">
-              </ul>
-              <div className="content">
-              </div>
-            </div>
+              <Navbar/>
+              <Route exact path="/" component={ Landing } />
+          </div>
+          </Router>
         )
       }
     }
