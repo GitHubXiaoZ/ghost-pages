434f17b8becb49d87bc514d3e215597d99169f46
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-04 00:25:58 -0400

    Update main.js

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
+import Register from "./components/auth/Register"
+import Login from "./components/auth/Login"
 
 class Main extends Component {
     render() {
@@ -10,8 +12,10 @@ class Main extends Component {
           <Router>
             <div className="main">
               <Navbar/>
-              <Route exact path="/" component={ Landing } />
-          </div>
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </div>
           </Router>
         )
       }
