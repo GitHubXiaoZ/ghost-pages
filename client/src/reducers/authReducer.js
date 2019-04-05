4e9a33efc7ca3054dc7c26c52582f9c54d041f02
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-05 01:23:26 -0400

    Create authReducer.js

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,33 @@
+/*imports actions*/
+import {
+    SET_CURRENT_USER,
+    USER_LOADING
+} from "../actions/types"
+
+const isEmpty = require("is-empty")
+
+/*default state*/
+const initialState = {
+    isAuth: false,
+    user: {},
+    loading: false
+}
+
+/*exports authentication user reducer*/
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuth: !isEmpty(action.payload),
+                user: action.payload
+            }
+        case USER_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state
+    }
+}
