3a7da97d054245cc9ef1a82f35d68e11565810ac
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-09 01:15:29 -0400

    Create Dashboard.js

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,53 @@
+/*imports*/
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+
+import { logoutUser } from "../../actions/authActions"
+
+/* Class: Dashboard
+ * User account page
+ * Contains functions such as logout
+ */
+class Dashboard extends Component {
+    onLogoutClick = e => {
+        e.preventDefault()
+        this.props.logoutUser()
+    }
+
+    render() {
+        const { user } = this.props.auth
+
+        return(
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <h4>
+                            Hello, {user.name}
+                        </h4>
+                        <button
+                         onClick={this.onLogoutClick}
+                         className="">
+                            Log out
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+Dashboard.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.func.isRequired
+} 
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+/*exports dashboard*/
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+) (Dashboard)
