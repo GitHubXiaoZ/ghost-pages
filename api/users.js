af5571f24c808927683db634c591f3e23486d5c3
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-11 01:03:14 -0400

    Update users.js

--- api/users.js
+++ api/users.js
@@ -4,27 +4,31 @@ const router = express.Router()
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const keys = require("../config/dbKeys")
+const passport = require("passport")
 
 const validRegInput = require("../validate/register")
 const validLogInput = require("../validate/login")
 
 const User = require("../models/User")
 
+//test route
+router.get("/test", (req, res) => res.json({ msg: "Valid route." }))
+
 /* POST api: register
  * register new users
  */
 router.post("/register", (req, res) => {
-    /*validates register inputs */
+    /*validates register inputs 
     const { errors, isValid } = validRegInput(req.body)
 
     if (!isValid) {
         return res.status(400).json(errors)
     }
-
+    */
     /*checks if email has already been used */
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
-            return res.status(400).json({ email: "Email has already been registered!"})
+            return res.status(400).json({ email: "Email has already been registered!" })
         } else {
             const newUser = new User({
                 name: req.body.name,
@@ -32,8 +36,10 @@ router.post("/register", (req, res) => {
                 password: req.body.password
             })
 
-            /*hash password */
-            bcrypt.genSalt(10, (err, salt) => {
+            /*hash password
+             *default: 10 rounds
+             */
+            bcrypt.genSalt((err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err
                     newUser.password = hash
@@ -51,19 +57,20 @@ router.post("/register", (req, res) => {
  * login in existing users
  * return jwt token
  */
-router.post("./login", (req, res) => {
+router.post("/login", (req, res) => {
      /*validates login inputs */
-     const { errors, isValid } = validLogInput(req.body)
-
+     //const { errors, isValid } = validLogInput(req.body)
+     /*
      if (!isValid) {
         return res.status(400).json(errors)
     }
-
+    */
     const email = req.body.email
     const password = req.body.password
 
     /*determine if user email exists */
     User.findOne({ email }).then(user => {
+        /*user check*/
         if (!user) {
             return res.status(404).json({ emailnotfound: "Email not registered!"})
         } 
@@ -71,12 +78,13 @@ router.post("./login", (req, res) => {
         /*password check*/
         bcrypt.compare(password, user.password).then(isEqual => {
             if (isEqual) {
+                /*jwt payload*/
                 const payload = {
                     id: user.id,
                     name: user.name
                 }
 
-                /*jwt sign in token*/
+                /*jwt sign token*/
                 jwt.sign (
                     payload,
                     keys.secretOrKey,
@@ -92,12 +100,24 @@ router.post("./login", (req, res) => {
                     }
                 )
             } else {
-                return res
-                    .status(400)
-                    .json({ passwordincorrect: "Password incorrect" })
+                return res.status(400).json({ passwordincorrect: "Password incorrect" })
             }
         })
     })
 })
+
+/* GET api: current
+ * returns the current user's id, name, and email
+ */
+router.get("/current", 
+    passport.authenticate("jwt", { session: false }), 
+    (req, res) => { 
+        res.json({
+            id: req.user.id,
+            name: req.user.name,
+            email: req.user.email
+        })
+    }
+)
 
 module.exports = router
