6af83c0a8bd52bd89ea8af94c5608789ae98685c
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-13 00:45:08 -0400

    Create Profile.js

---
+++ models/Profile.js
@@ -0,0 +1,36 @@
+/*imports*/
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 25
+    },
+    location: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    social: {
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+/*exports*/
+module.exports = Profile = mongoose.model("profile", ProfileSchema)
