8b345afc12434397ec215e4db8e8595d7d6a988d
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-13 00:45:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 
 const users = require("./api/users")
+const profile =require("./api/profile")
 
 const port = process.env.PORT || 3001
 
@@ -33,5 +34,6 @@ require("./config/passport")(passport)
 app.get("/", (req, res) => res.send("o l l e h"))
 
 app.use("/api/users", users)
+app.use("/api/profile", profile)
 
 app.listen(port, () => console.log(`Server running on port ${port}.`))
