1703a043a557470f68dedaeab38ea3ef626fc5a2
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-15 01:17:17 -0400

    Update profile.js

--- validate/profile.js
+++ validate/profile.js
@@ -2,17 +2,20 @@
 const Validator = require("validator")
 const isEmpty = require("is-empty")
 
+/*exports*/
 module.exports = validateProfileInput = data => {
     let errors = {}
 
+    /*sets fields to empty string if no input is given*/
     data.handle = !isEmpty(data.handle) ? data.handle : ""
     data.status = !isEmpty(data.status) ? data.status : ""
 
+    /*validates profile handle*/
     if (Validator.isEmpty(data.handle)) {
         errors.handle = "Profile handle required!"
     }
 
-    if (Validator.isLength(data.handle, { min: 2, max: 20 })) {
+    if (!Validator.isLength(data.handle, { min: 2, max: 20 })) {
         errors.handle = "Profile handle length must be between 2 and 20 characters!"
     }
 
@@ -20,6 +23,7 @@ module.exports = validateProfileInput = data => {
         errors.status = "Profile status required!"
     }
 
+    /*validate social media links*/
     if (!isEmpty(data.twitter)) {
         if (!Validator.isURL(data.twitter)) {
           errors.twitter = 'Not a valid URL'
@@ -32,6 +36,7 @@ module.exports = validateProfileInput = data => {
         }
     }
 
+    /*returns errors*/
     return {
         errors,
         isValid: isEmpty(errors)
