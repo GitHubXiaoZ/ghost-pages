9bd8a2a1a21fc90427884fbafe5c7c16b32dcc2a
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-15 01:16:42 -0400

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -12,50 +12,6 @@ const Profile = require("../models/Profile")
 /*test route*/
 router.get("/test", (req, res) => res.json({ msg: "Profile route -- working." }))
 
-/* POST api: profile
- * creates the user's profile
- */
-router.post("/",
-    passport.authenticate("jwt", { session: false }),
-    (req, res) => {
-        const { errors, isValid } = validateProfileInput(req.body)
-
-        if (!isValid) {
-            return res.status(400).json(errors)
-        }
-
-        const profileInfo = {}
-        profileInfo.user = req.user.id
-        if (req.body.handle) profileInfo.handle = req.body.handle
-        if (req.body.location) profileInfo.location = req.body.location
-        if (req.body.bio) profileInfo.bio = req.body.bio
-
-        profileInfo.social = {}
-        if (req.body.twitter) profileInfo.social.twitter = req.body.twitter
-        if (req.body.facebook) profileInfo.social.facebook = req.body.facebook
-
-        Profile.findOne({ user: req.user.id })
-            .then(profile => {
-                if (profile) {
-                    Profile.findOneAndUpdate(
-                        { user: req.body.id}, 
-                        { $set: profileInfo },
-                        { new: true }
-                    ).then(profile => res.json(profile))
-                } else {
-                    Profile.findOne({ handle: profileInfo.handle }).then(profile => {
-                        if (profile) {
-                            errors.handle = "Handle already exists!"
-                            res.status(400).json(errors)
-                        }
-
-                        new Profile(profileInfo).save().then(profile => res.json(profile))
-                    })
-                }
-            })
-    }
-)
-
 /* GET api: profile
  * returns the current user's profile
  */
@@ -77,5 +33,49 @@ router.get("/",
         }
 )
 
-module.exports = router
+/* POST api: profile
+ * creates the user's profile
+ */
+router.post("/",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body)
 
+        if (!isValid) {
+            return res.status(400).json(errors)
+        }
+
+        const profileInfo = {}
+        profileInfo.user = req.user.id
+        if (req.body.handle) profileInfo.handle = req.body.handle
+        if (req.body.location) profileInfo.location = req.body.location
+        if (req.body.bio) profileInfo.bio = req.body.bio
+        if (req.body.status) profileInfo.status = req.body.status
+
+        profileInfo.social = {}
+        if (req.body.twitter) profileInfo.social.twitter = req.body.twitter
+        if (req.body.facebook) profileInfo.social.facebook = req.body.facebook
+
+        Profile.findOne({ user: req.user.id })
+            .then(profile => {
+                if (profile) {
+                    Profile.findOneAndUpdate(
+                        { user: req.user.id }, 
+                        { $set: profileInfo },
+                        { new: true }
+                     ).then(profile => res.json(profile))
+                } else {
+                    Profile.findOne({ handle: profileInfo.handle }).then(profile => {
+                        if (profile) {
+                            errors.handle = "Handle already exists!"
+                            res.status(400).json(errors)
+                        }
+
+                        new Profile(profileInfo).save().then(profile => res.json(profile))
+                    })
+                }
+            })
+    }
+)
+
+module.exports = router
