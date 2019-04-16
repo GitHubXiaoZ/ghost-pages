3c149251374604e82983441a04d910c65a2f567e
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-16 01:57:07 -0400

    Create post.js

---
+++ validate/post.js
@@ -0,0 +1,24 @@
+/*imports*/
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+/*exports*/
+module.exports = validatePostInput = data => {
+    let errors = {}
+
+    data.text = !isEmpty(data.text) ? data.text : ""
+
+    if (Validator.isEmpty(data.text)) {
+        errors.text = "Post must include some content!"
+    }
+
+    if (!Validator.isLength(data.text, { min: 1, max: 255 })) {
+        errors.text = "Post cannot exceed 300 characters!"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
