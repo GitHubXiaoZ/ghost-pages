d92dc6ab37a47276367179148248305a02062c13
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-16 01:56:45 -0400

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+/*imports*/
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            data: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ]
+})
+
+/*exports*/
+module.exports = Post = mongoose.model("post", PostSchema)
