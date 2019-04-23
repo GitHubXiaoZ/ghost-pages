aefb71f81bdd901ce110ae01bb7d41b8e94c524b
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-23 01:18:51 -0400

    Update typesActions.js

--- client/src/actions/typesActions.js
+++ client/src/actions/typesActions.js
@@ -1,4 +1,13 @@
 /*exports possible actions*/
-export const GET_ERRORS = "GET_ERRORS";
-export const USER_LOADING = "USER_LOADING";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_ERRORS = "GET_ERRORS"
+export const USER_LOADING = "USER_LOADING"
+export const SET_CURRENT_USER = "SET_CURRENT_USER"
+
+export const GET_PROFILE = "GET_PROFILE"
+export const GET_PROFILES = "GET_PROFILES"
+
+export const PROFILE_LOADING = "PROFILE_LOADING"
+export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND"
+export const RESET_CURRENT_PROFILE = "RESET_CURRENT_PROFILE"
+
+
