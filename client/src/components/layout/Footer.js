846c72f78c4fcc04753435fb97e8018ab88790b2
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-26 01:21:45 -0400

    Create Footer.js

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+/*import*/
+import React from "react"
+
+/*export*/
+export default () => {
+    return (
+      <footer className="footer">
+        Copyright &copy; {new Date().getFullYear()} &nbsp; Ghost Pages
+      </footer>
+    );
+  };
