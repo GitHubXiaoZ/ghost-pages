6296af8b6e91afab175346c5318df050472a9421
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-27 00:24:03 -0400

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -55,7 +55,7 @@ class Login extends Component {
 
         return (
             <div className="login">
-                <div className="">
+                <div className="form">
                     <div className="home">
                         <Link to="/">
                             Home
