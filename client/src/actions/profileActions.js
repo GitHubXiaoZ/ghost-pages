4b3dc680ea0cc61b65c646aa796562bb4a6f82bb
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-28 02:33:43 -0400

    Update profileActions.js

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,12 +1,12 @@
 /*imports*/
 import axios from "axios"
 
-import { GET_PROFILE, PROFILE_LOADING, RESET_CURRENT_PROFILE, GET_ERRORS } from "./typesActions"
+import { GET_PROFILE, PROFILE_LOADING, SET_CURRENT_USER, RESET_CURRENT_PROFILE, GET_ERRORS } from "./typesActions"
 
 /*create new profile*/
 export const newProfile = (profileData, history) => dispatch => {
     axios
-        .post("/api/profiles")
+        .post("/api/profiles", profileData)
         .then(res => history.push("/dashboard"))
         .catch(err => 
             dispatch({
@@ -46,4 +46,21 @@ export const resetCurrentProfile = () => {
     return {
         type: RESET_CURRENT_PROFILE
     }
+}
+
+/*delete profile action*/
+export const deleteAccount = () => dispatch => {
+    axios
+        .delete("/api/profile")
+        .then(res =>
+            dispatch({
+                type: SET_CURRENT_USER,
+                payload: {}
+            })
+        ).catch(err=> 
+            dispatch({
+                type: GET_ERRORS,
+                payload:err.response.data
+            })
+        )
 }
