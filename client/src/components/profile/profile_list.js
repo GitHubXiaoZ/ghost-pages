e955e8c80da1be32200fe1b48493f072cb199168
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-04-29 02:20:35 -0400

    Create profile_list.js

---
+++ client/src/components/profile/profile_list.js
@@ -0,0 +1,57 @@
+/*imports*/
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+
+import { getProfileList } from "../../actions/profileActions"
+
+/* Class: ProfileList
+ * List of Profiles
+ * Allows user to search through profiles
+ */
+class ProfileList extends Component {
+    componentDidMount() {
+        this.props.getProfileList()
+    }
+
+    render() {
+        const { profiles, loading } = this.props.profile
+        let profileItems
+
+        if (profiles == null || loading) {
+            profileItems = <h3>transmitting...</h3>
+        } else {
+            if (profiles.length > 0) {
+                profileItems = <h1>User Profiles</h1>
+            } else {
+                profileItems = <h3>No profiles found!</h3>
+            }
+        }
+        return (
+            <div className="profiles">
+                <h1>
+                    User Profiles
+                </h1>
+                <p>
+                    Connect with another user.
+                </p>
+                {profileItems}
+            </div>
+        )
+    }
+}
+
+ProfileList.propTypes = {
+    getProfileList: PropTypes.func.isRequired,
+    profile:PropTypes.object.isRequired
+}
+
+const mapStateToProp = state => ({
+    profile: state.profile
+})
+
+/*export profilelist*/
+export default connect(
+    mapStateToProp,
+    { getProfileList }
+) (ProfileList)
