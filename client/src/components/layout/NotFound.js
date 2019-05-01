2bfeb0752db69fdcfe324d226cfdac37901070b5
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-01 02:02:38 -0400

    Create NotFound.js

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,20 @@
+//import
+import React, { Component } from "react";
+
+/*
+ * Class: NotFound
+ * 404 page
+ */
+class NotFound extends Component {
+    //renders 404 page
+    render() {
+        return (
+            <div className="404">
+            <h2>404</h2>
+            <p>Page not found.</p>
+            </div>
+        );
+    }
+}
+//exports 404 page
+export default NotFound;
