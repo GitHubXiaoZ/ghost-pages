64133cac0a76530d8a2351a40c591b1b372fb409
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-02 01:08:33 -0400

    Create profile_content.js

---
+++ client/src/components/profile/profile_content.js
@@ -0,0 +1,35 @@
+/*imports*/
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import isEmpty from "is-empty"
+
+/* Class: ProfileContent
+ * Profile body component
+ * Content of user profile containing name, status, location, and socials
+ */
+class ProfileContent extends Component {
+    render() {
+        const { profile } = this.props
+        const username = profile.user.name
+
+        return (
+            <div className="content">
+                <h2>{username}</h2>
+                <p>
+                    {isEmpty(profile.bio) ? (
+                        <span><i>{username} has a dark and mysterious past.</i></span>
+                    ) : (
+                        <span>{profile.bio}</span>
+                    )}
+                </p>
+            </div>
+        )
+    }
+}
+
+ProfileContent.propType = {
+    profile: PropTypes.object.isRequired
+}
+
+/*exports profilecontent*/
+export default ProfileContent
