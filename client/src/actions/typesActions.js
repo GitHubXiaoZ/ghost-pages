24798876adb83b65c4feeff2a2755c8922bedb7f
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-03 02:28:47 -0400

    Update typesActions.js

--- client/src/actions/typesActions.js
+++ client/src/actions/typesActions.js
@@ -10,4 +10,10 @@ export const PROFILE_LOADING = "PROFILE_LOADING"
 export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND"
 export const RESET_CURRENT_PROFILE = "RESET_CURRENT_PROFILE"
 
+export const GET_POST = "GET_POST"
+export const GET_POST_LIST = "GET_POST_LIST"
+
+export const POST_LOADING = "POST_LOADING"
+export const ADD_POST = "ADD_POST"
+export const DELETE_POST = "DELETE_POST"
 
