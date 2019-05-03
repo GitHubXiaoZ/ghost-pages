294fd79bc95135e5682219eba3563b68563f08d5
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-03 02:28:54 -0400

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,13 @@
 import { combineReducers } from "redux"
 import authReducer from "./authReducer"
 import errorReducer from "./errorReducer"
+import profileReducer from "./profileReducer"
+import postReducer from "./postReducer"
 
 /*export all reducers*/
 export default combineReducers({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    profile: profileReducer,
+    post: postReducer
 })
