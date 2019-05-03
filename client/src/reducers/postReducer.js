944fd5ae4c53c2b60724518388f4eaa0c6afc68b
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-03 02:25:54 -0400

    Create postReducers.js

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+/*inital state*/
+const initialState = {
+    post: [],
+    post_list: {},
+    loading: false
+}
+
+/*exports post reducer*/
+export default function(state = initialState, action) {
+    switch (action.type) {
+        default: 
+            return state 
+    }
+}
