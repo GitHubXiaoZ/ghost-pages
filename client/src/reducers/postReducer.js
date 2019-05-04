3844ba45c5db646907099c36c05fb86ac527ff16
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-04 00:32:46 -0400

    Update postReducer.js

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,3 +1,11 @@
+/*imports*/
+import {
+    ADD_POST,
+    GET_POST,
+    GET_POST_LIST,
+    POST_LOADING
+} from "../actions/typesActions"
+
 /*inital state*/
 const initialState = {
     post: [],
@@ -8,6 +16,28 @@ const initialState = {
 /*exports post reducer*/
 export default function(state = initialState, action) {
     switch (action.type) {
+        case POST_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: action.payload,
+                loading: false
+            }
+        case GET_POST_LIST:
+            return {
+                ...state,
+                post_list: action.payload,
+                loading: false
+            }
+        case ADD_POST:
+            return {
+                ...state,
+                post_list: [action.payload, ...state.post_list]
+            }
         default: 
             return state 
     }
