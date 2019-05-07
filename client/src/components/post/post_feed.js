0b2af4cb8e0208568f820b7a3035a80f97dbba23
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-07 00:56:38 -0400

    Create post_feed.js

---
+++ client/src/components/post/post_feed.js
@@ -0,0 +1,23 @@
+/*imports*/
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import PostItem from "./post_item"
+
+/* Class: PostFeed
+ * Collection of posts
+ * Render a postitem for each post in post_list
+ */
+class PostFeed extends Component {
+    render() {
+        const { post_list } = this.props
+        
+        return (post_list.map(post => <PostItem key={post._id} post={post}/>))
+    }
+}
+
+PostFeed.propTypes = {
+    post_list: PropTypes.object.isRequired
+}
+
+/*export postfeed*/
+export default PostFeed
