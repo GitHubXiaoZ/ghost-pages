4a74e925b085e5300da157010459ba9f1b554a0d
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-10 02:30:07 -0400

    Create comment_feed.js

---
+++ client/src/components/post/comment/comment_feed.js
@@ -0,0 +1,24 @@
+/*imports*/
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import CommentItem from "./comment_item"
+
+/* Class: CommentFeed
+ * Collection of comments
+ * Render a commentitem for each comment in comment_list
+ */
+class CommentFeed extends Component {
+    render() {
+        const { comment_list, post_id } = this.props
+        
+        return comment_list.map(comment => <CommentItem key={comment._id} comment={comment} post_id={post_id}/>)
+    }
+}
+
+CommentFeed.propTypes = {
+    post_id: PropTypes.string.isRequired,
+    comment: PropTypes.object.isRequired
+}
+
+/*export commentfeed*/
+export default CommentFeed
