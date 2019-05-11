41aa9ef764d1fe7c3a0692b12cda83babdf53b19
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-11 02:13:25 -0400

    Update errorReducer.js

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,5 +1,5 @@
 /*imports action*/
-import { GET_ERRORS } from "../actions/typesActions"
+import { GET_ERRORS, RESET_ERRORS } from "../actions/typesActions"
 
 /*default*/
 const initialState = {}
@@ -9,6 +9,8 @@ export default function(state = initialState, action) {
     switch (action.type) {
         case GET_ERRORS:
             return action.payload
+        case RESET_ERRORS:
+            return {}
         default:
             return state 
     }
