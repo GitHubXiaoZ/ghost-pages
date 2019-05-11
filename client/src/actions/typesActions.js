603f4fe32a5e385cb555b521daaa3317ccec20f4
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-11 02:13:12 -0400

    Update typesActions.js

--- client/src/actions/typesActions.js
+++ client/src/actions/typesActions.js
@@ -1,5 +1,6 @@
 /*exports possible actions*/
 export const GET_ERRORS = "GET_ERRORS"
+export const RESET_ERRORS = "RESET_ERRORS"
 export const USER_LOADING = "USER_LOADING"
 export const SET_CURRENT_USER = "SET_CURRENT_USER"
 
