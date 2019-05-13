1443a4c3752d2328340d4bb62cc8ef6cbb7b18d6
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-13 02:29:21 -0400

    Update profile_header.js

--- client/src/components/profile/profile_header.js
+++ client/src/components/profile/profile_header.js
@@ -4,7 +4,7 @@ import isEmpty from "is-empty"
 
 /* Class: ProfileHeader
  * Profile header component
- * Header of user profile containing name, status, location, and socials
+ * Header of user profile containing status and location
  */
 class ProfileHeader extends Component {
     render() {
@@ -12,30 +12,9 @@ class ProfileHeader extends Component {
 
         return (
             <div className="head">
-                <h1>{profile.user.name}</h1>
                 <p>{profile.status}</p>
                 <p>
                     {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
-                </p>
-                <p>
-                    {isEmpty(profile.social && profile.social.twitter) ? null : (
-                        <a 
-                            className="twitter"
-                            href={profile.social.twitter}
-                            >
-                            Twitter
-                        </a>
-                    )}
-                </p>
-                <p>
-                    {isEmpty(profile.social && profile.social.facebook) ? null : (
-                        <a 
-                            className="facebook"
-                            href={profile.social.facebook}
-                            >
-                            Facebook
-                        </a>
-                    )}
                 </p>
             </div>
         )
