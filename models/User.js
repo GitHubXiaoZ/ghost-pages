3eca2d3e668b6c356281ac29b1b66ab15f7474e6
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-14 03:05:28 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -15,9 +15,6 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    pfp: {
-        type: String
-    },
     date: {
         type: Date,
         default: Date.now
