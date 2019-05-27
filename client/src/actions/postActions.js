d3d229783b96910b8233a30d645c3086e660636a
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-27 02:43:00 -0400

    Update postActions.js

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -137,7 +137,7 @@ export const addComment = (post_id, commentData) => dispatch => {
 /*add a comment to post*/
 export const deleteComment = (post_id, comment_id) => dispatch => {
     axios
-        .post(`/api/posts/comment/${post_id}/${comment_id}`)
+        .delete(`/api/posts/comment/${post_id}/${comment_id}`)
         .then(res => 
             dispatch({
                 type: GET_POST,
