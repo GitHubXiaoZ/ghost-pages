a515ee1bb178f3dc93071b6a278b2f12ced03f6f
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-05-30 02:18:19 -0400

    Create millsToDaysHoursMinutes.js

---
+++ client/src/utils/millsToDaysHoursMinutes.js
@@ -0,0 +1,44 @@
+/**
+ * function millsToDaysHoursMinutes
+ * @param milliseconds
+ * returns days, hours, minutes, and seconds based on time lapsed (in milliseconds)
+ */
+const millsToDaysHoursMinutes = ms => {
+    //total time lapsed in days, hours, minutes, seconds
+    const t_seconds = Math.floor(ms / 1000)
+    const t_minutes = Math.floor(t_seconds / 60)
+    const t_hours = Math.floor(t_minutes / 60)
+    const days = Math.floor(t_hours / 24)
+    //actual hours, minutes, seconds 
+    const hours = parseInt(t_hours % 60)
+    const minutes = parseInt(t_minutes % 60)
+    const seconds = parseInt(t_seconds % 60)
+
+    if (days > 0) {
+        if (days === 1) {
+            return days + "d ago"
+        }
+    return days + "ds ago" 
+    }
+    if (hours > 0) {
+        if (hours === 1) {
+            return hours + "h ago"
+        }
+        return hours + "hs ago"
+    }
+    if (minutes > 0) {
+        if (minutes === 1) {
+            return minutes + "min ago"
+        }
+        return minutes + "mins ago"
+    }
+    if (seconds > 0) {
+        if (seconds < 15) {
+            return "Just now!"
+        }
+        return seconds + "s ago"
+    }
+}
+
+/*export*/
+export default millsToDaysHoursMinutes
