f4280ba77de609d6327cb2a284c3119a2f85fe14
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-06-04 00:48:44 -0400

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -7,6 +7,9 @@ const PostSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "users"
     },
+    title: {
+        type: String
+    },
     text: {
         type: String,
         required: true
