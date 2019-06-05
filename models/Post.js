37e7dbb93cdbf18e08220198cae473f774af75c7
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-06-05 03:16:35 -0400

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,11 @@ const PostSchema = new Schema({
     name: {
         type: String
     },
+    tags: [
+        {
+            text: String
+        }
+    ],
     likes: [
         {
             user: {
