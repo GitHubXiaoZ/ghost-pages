fc153aa511b6674744c17c64b30be287f35846b2
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-06-12 02:56:45 -0400

    Update profile.js

--- validate/profile.js
+++ validate/profile.js
@@ -5,14 +5,19 @@ const isEmpty = require("is-empty")
 /*exports*/
 module.exports = validateProfileInput = data => {
     let errors = {}
+    const regex = /^[a-zA-z0-9-]+$/g
 
     /*sets fields to empty string if no input is given*/
     data.handle = !isEmpty(data.handle) ? data.handle : ""
     data.status = !isEmpty(data.status) ? data.status : ""
 
+    const valid_profile = data.handle.match(regex)
+
     /*validates profile handle*/
     if (Validator.isEmpty(data.handle)) {
         errors.handle = "Profile handle required!"
+    } else if (!valid_profile) {
+        errors.handle = "Profile handle can only include alphanumeric characters, _(underscore), and -(hyphen)!"
     }
 
     if (!Validator.isLength(data.handle, { min: 2, max: 20 })) {
