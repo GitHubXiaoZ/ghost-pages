46f392e27ccf55f4d16ce87ccfe4917e3945d118
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-07-18 01:10:48 -0400

    Create post_list_filter.js

---
+++ client/src/components/post/post_list_filter.js
@@ -0,0 +1,68 @@
+/*imports*/
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
+
+import Loading from "../layout/Loading"
+import PostFeed from "./post_feed"
+import { getPostListByUser,
+         getPostListByHandle, 
+         getPostListByTag } from "../../actions/postActions"
+
+/* Class: PostList
+ * Post component
+ * Displays post feed containing all posts
+ */
+class PostList extends Component {
+    componentDidMount() {
+        if (this.props.match.params.id) {
+            this.props.getPostListByUser(this.props.match.params.id)
+        } else if (this.props.match.params.handle) {
+            this.props.getPostListByHandle(this.props.match.params.handle)
+        } else if (this.props.match.params.tag) {
+            this.props.getPostListByTag(this.props.match.params.tag)
+        }
+    }
+
+    render() {
+        const { post_list, loading } = this.props.post
+        let postFeed
+
+        if (post_list === null || loading) {
+            postFeed = <Loading/>
+        } else {
+            postFeed = <PostFeed post_list={post_list}/>
+        }
+
+        return(
+            <div className="post">
+                <div className="user-feed">
+                    <Link to="/dashboard">Return</Link>
+                </div>
+                {postFeed}
+            </div>
+        )
+    }
+}
+
+PostList.propTypes = {
+    post: PropTypes.object.isRequired,
+    getPostListByUser: PropTypes.func.isRequired,
+    getPostListByHandle: PropTypes.func.isRequired,
+    getPostListByTag: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    post: state.post,
+    auth: state.auth
+})
+
+/*exports postlist*/
+export default connect(
+    mapStateToProps,
+     { getPostListByUser,
+       getPostListByHandle,
+       getPostListByTag }
+) (PostList)
