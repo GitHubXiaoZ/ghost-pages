27c17492df96ebd701e6790c358c7403ef31838e
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-07-20 00:06:44 -0400

    Update types.js

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,10 +11,12 @@ export const PROFILE_LOADING = "PROFILE_LOADING"
 export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND"
 export const RESET_CURRENT_PROFILE = "RESET_CURRENT_PROFILE"
 
+export const POST_LOADING = "POST_LOADING"
+
 export const GET_POST = "GET_POST"
 export const GET_POST_LIST = "GET_POST_LIST"
-
-export const POST_LOADING = "POST_LOADING"
 export const ADD_POST = "ADD_POST"
 export const DELETE_POST = "DELETE_POST"
 
+export const GET_TAG = "GET_TAG"
+export const GET_TAG_LIST = "GET_TAG_LIST"
