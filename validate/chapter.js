c5726701656b3bfca66220f64f19208d6ff2e092
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-03 00:30:57 -0400

    Create chapter.js

---
+++ validate/chapter.js
@@ -0,0 +1,25 @@
+/*imports*/
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+/*exports*/
+module.exports = validateChapterInput = data => {
+    let errors = {}
+
+    data.title = !isEmpty(data.title) ? data.title : ""
+    data.text = !isEmpty(data.text) ? data.text : ""
+
+    if (Validator.isEmpty(data.title)) {
+        errors.title = "Title cannot be empty!"
+    }
+
+    if (Validator.isEmpty(data.text)) {
+        errors.text = "Chapter cannot be empty!"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
