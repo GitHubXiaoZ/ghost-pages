5b3773bfcf6da9a2f4d7e81801e617de7dfb46a0
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-03 00:31:16 -0400

    Create novel.js

---
+++ validate/novel.js
@@ -0,0 +1,30 @@
+/*imports*/
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+/*exports*/
+module.exports = validateNovelInput = data => {
+    let errors = {}
+
+    data.title = !isEmpty(data.title) ? data.title : ""
+    data.summary = !isEmpty(data.summary) ? data.summary : ""
+    data.status = !isEmpty(data.status) ? data.status : ""
+
+    if (Validator.isEmpty(data.title)) {
+        errors.title = "Title cannot be empty!"
+    }
+
+    if (Validator.isEmpty(data.summary)) {
+        errors.summary = "Summary cannot be empty!"
+    }
+
+    if (Validator.isEmpty(data.status)) {
+        errors.status = "Status cannot be empty!"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
