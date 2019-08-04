f8e9f7b01ae427454253e9b5af526ef3be6a2bfb
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-04 01:38:07 -0400

    Update novel.js

--- validate/novel.js
+++ validate/novel.js
@@ -6,6 +6,7 @@ const isEmpty = require("is-empty")
 module.exports = validateNovelInput = data => {
     let errors = {}
 
+    /*sets fields to empty string if no input is given*/
     data.title = !isEmpty(data.title) ? data.title : ""
     data.summary = !isEmpty(data.summary) ? data.summary : ""
     data.status = !isEmpty(data.status) ? data.status : ""
