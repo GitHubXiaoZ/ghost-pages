ed315aba8cb7631bcdf761a91f9038b2dd00696a
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-12 01:27:47 -0400

    Update chapters.js

--- api/chapters.js
+++ api/chapters.js
@@ -12,3 +12,16 @@ const validNovelInput = require("../validate/novel")
 
 /*test route*/
 router.get("/test", (req, res) => res.json({ msg: "Chapter route -- working." }))
+
+/* GET api: chapters
+ * returns all chapters
+ */
+router.get("/", (req, res) => {
+    //sorts by query
+    //default by new
+    let sort = req.query.sort ? req.query.sort : -1
+    Chapter.find()
+        .sort({ date: sort })
+        .then(chapters => res.json(chapters))
+        .catch(err => res.status(404).json({ nochapters: "Chapters have not been created!" }))
+})
