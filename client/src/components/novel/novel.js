9e47fc9d7fa7fe42aa2957f9ccb79ac2e0557420
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-22 15:05:49 -0400

    Create novel.js

---
+++ client/src/components/novel/novel.js
@@ -0,0 +1,60 @@
+/*imports*/
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
+
+import Loading from "../layout/Loading"
+import NovelItem from "./novel_item"
+import { getNovel } from "../../actions/novelActions"
+
+/* Class: Novel
+ * Novel component
+ * Displays a single novel from novel feed
+ */
+class Novel extends Component {
+    componentDidMount() {
+        this.props.getNovel(this.props.match.params.id)
+    }
+
+    render() {
+        const { novel, loading } = this.props.novel
+        let library
+
+        if (novel === null || loading || Object.keys(novel).length === 0) {
+            library = <Loading/>
+        } else {
+            library =(
+                <div>
+                    <NovelItem novel={novel}/>
+                </div>
+            )
+        }
+
+        return (
+            <div className="post">
+                <div className="thread">
+                    <Link to="/dashboard">Return</Link>
+                </div>
+                {library}
+            </div>
+        )
+    }
+}
+
+Novel.propTypes = {
+    getNovel: PropTypes.func.isRequired,
+    novel: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    novel: state.novel,
+    auth: state.auth
+})
+
+/*export post*/
+export default connect(
+    mapStateToProps,
+    { getNovel }
+) (Novel)
