5ea6ebe43081a804944147602769450c36af02c1
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-25 01:28:25 -0400

    Create novel_library.js

---
+++ client/src/components/novel/novel_library.js
@@ -0,0 +1,23 @@
+/*imports*/
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import NovelItem from "./post_item"
+
+/* Class: NovelLibrary
+ * Array of novels
+ * Render a novel item for each novel in novel list
+ */
+class NovelLibrary extends Component {
+    render() {
+        const { novel_list } = this.props
+        
+        return novel_list.map(novel => <NovelItem key={novel._id} novel={novel}/>)
+    }
+}
+
+NovelLibrary.propTypes = {
+    novel_list: PropTypes.array.isRequired
+}
+
+/*export postfeed*/
+export default NovelLibrary
