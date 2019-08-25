35ad739d0caca3642b30512f12cd2400e43f8f24
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-25 01:28:36 -0400

    Create novel_list.js

---
+++ client/src/components/novel/noveLlist.js
@@ -0,0 +1,59 @@
+/*imports*/
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
+
+import Loading from "../layout/Loading"
+import NovelForm from "./novel_form"
+import NovelLibrary from "./novel_library"
+import { getNovelList } from "../../actions/novelActions"
+
+/* Class: NovelList
+ * Novel component
+ * Displays novel library containing all novels
+ */
+class NovelList extends Component {
+    componentDidMount() {
+        this.props.getNovelList()
+    }
+
+    render() {
+        const { novel_list, loading } = this.props.novel
+        const { isAuth } = this.props.auth
+        let library
+
+        if (novel_list === null || loading) {
+            library = <Loading/>
+        } else {
+            library = <NovelLibrary novel_list={novel_list}/>
+        }
+
+        return(
+            <div className="novel">
+                <div className="user-feed">
+                    <Link to="/dashboard">Return</Link>
+                </div>
+                {isAuth ? <NovelForm/> : null}
+                {library}
+            </div>
+        )
+    }
+}
+
+NovelList.propTypes = {
+    novel: PropTypes.object.isRequired,
+    getNovelList: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    novel: state.novel,
+    auth: state.auth
+})
+
+/*exports novellist*/
+export default connect(
+    mapStateToProps,
+     { getNovelList }
+) (NovelList)
