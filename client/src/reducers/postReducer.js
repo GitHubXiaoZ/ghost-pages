bb03b52db07ac0742376abc599b030a9d298efbf
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-08-31 21:59:05 -0400

    Update postReducer.js

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -4,8 +4,8 @@ import {
     GET_POST,
     GET_POST_LIST,
     GET_TAG_LIST,
-    POST_LOADING,
-    DELETE_POST } from "../actions/types"
+    DELETE_POST,
+    POST_LOADING } from "../actions/types"
 
 /*inital state*/
 const initialState = {
