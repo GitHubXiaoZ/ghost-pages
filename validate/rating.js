c4759bd6e5c58e4f930a7307ec9be7029c8dcfd7
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-09-01 17:55:32 -0400

    Create rating.js

---
+++ validate/rating.js
@@ -0,0 +1,23 @@
+/*imports*/
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+/*exports*/
+module.exports = validRatingInput = data => {
+    let errors = {}
+
+    /*sets fields
+     *rating
+     *to empty string if no input is given*/
+    data.rating = !isEmpty(data.rating) ? data.rating : ""
+    
+    if (!Validator.isInt(data.rating, { min: 1, max: 5 })) {
+        errors.rating = "Ratings must be between a value of 1 and 5!"
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
