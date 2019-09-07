cc4f4e16dea2bf4ec5aded19cf3b5ea6404f5a65
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-09-07 00:14:49 -0400

    Update rating.js

--- validate/rating.js
+++ validate/rating.js
@@ -11,6 +11,10 @@ module.exports = validRatingInput = data => {
      *to empty string if no input is given*/
     data.rating = !isEmpty(data.rating) ? data.rating : ""
     
+    if (Validator.isEmpty(data.rating)) {
+        errors.rating = "Rating cannot be empty!"
+    }
+
     if (!Validator.isInt(data.rating, { min: 1, max: 5 })) {
         errors.rating = "Ratings must be between a value of 1 and 5!"
     }
