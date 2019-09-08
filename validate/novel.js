94546a9d3a813daed0cbfb4d205d9537e1a7673e
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-09-08 13:05:44 -0400

    Update novel.js

--- validate/novel.js
+++ validate/novel.js
@@ -19,6 +19,7 @@ module.exports = validateNovelInput = data => {
         errors.title = "Title cannot be empty!"
     }
 
+    //title should not be longer than 250 characters
     if (!Validator.isLength(data.title, { max: 250 })) {
         errors.title = "Title cannot exceed 250 characters!"
     }
@@ -26,7 +27,8 @@ module.exports = validateNovelInput = data => {
     if (Validator.isEmpty(data.synopsis)) {
         errors.synopsis = "Synopsis cannot be empty!"
     }
-
+    
+    //synopsis should not be longer than 1000
     if (!Validator.isLength(data.synopsis, { max: 1000 })) {
         errors.synopsis = "Synopsis cannot exceed 1000 characters!"
     }
