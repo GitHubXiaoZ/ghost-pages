b9e5cea9b4da94fd59d8768692122f6bf04e3f95
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-09-12 00:31:36 -0400

    Update chapters.js

--- api/chapters.js
+++ api/chapters.js
@@ -8,8 +8,6 @@ const { Chapter } = require("../models/Novel")
 const Profile = require("../models/Profile")
 const User = require("../models/User")
 
-const validNovelInput = require("../validate/novel")
-
 /*test route*/
 router.get("/test", (req, res) => res.json({ msg: "Chapter route -- working." }))
 
