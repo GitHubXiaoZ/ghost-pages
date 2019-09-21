232000c02392c0aa783072dae01dc77ecc06dec5
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-09-21 00:27:43 -0400

    Update types.js

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,16 +1,22 @@
 /*exports possible actions*/
+//errors
 export const GET_ERRORS = "GET_ERRORS"
 export const RESET_ERRORS = "RESET_ERRORS"
+
+//users
 export const USER_LOADING = "USER_LOADING"
 export const SET_CURRENT_USER = "SET_CURRENT_USER"
+
+//profiles
+export const PROFILE_LOADING = "PROFILE_LOADING"
 
 export const GET_PROFILE = "GET_PROFILE"
 export const GET_PROFILE_LIST = "GET_PROFILE_LIST"
 
-export const PROFILE_LOADING = "PROFILE_LOADING"
 export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND"
 export const RESET_CURRENT_PROFILE = "RESET_CURRENT_PROFILE"
 
+//posts
 export const POST_LOADING = "POST_LOADING"
 
 export const GET_POST = "GET_POST"
@@ -18,6 +24,7 @@ export const GET_POST_LIST = "GET_POST_LIST"
 export const ADD_POST = "ADD_POST"
 export const DELETE_POST = "DELETE_POST"
 
+//novels
 export const NOVEL_LOADING = "NOVEL_LOADING"
 
 export const GET_NOVEL = "GET_NOVEL"
@@ -25,4 +32,5 @@ export const GET_NOVEL_LIST = "GET_NOVEL_LIST"
 export const ADD_NOVEL = "ADD_NOVEL"
 export const DELETE_NOVEL = "DELETE_NOVEL"
 
+//tags
 export const GET_TAG_LIST = "GET_TAG_LIST"
