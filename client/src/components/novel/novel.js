ca53b9544d32829d9dee0d532f1e2f6fa828b9c2
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-11-19 02:42:13 -0500

    Update novel.js

--- client/src/components/novel/novel.js
+++ client/src/components/novel/novel.js
@@ -13,7 +13,7 @@ import CommentFeed from "../post/comment/comment_feed"
 
 /* Class: Novel
  * Novel component
- * Display a novel from novel_library
+ * Display a novel
  */
 class Novel extends Component {
     componentDidMount() {
