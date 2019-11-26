bd18a8056548e0f6ed4191a6201b8dd7585c4377
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-11-26 01:50:49 -0500

    Update post.js

--- client/src/components/post/post.js
+++ client/src/components/post/post.js
@@ -13,7 +13,7 @@ import CommentFeed from "./comment/comment_feed"
 
 /* Class: Post
  * Post component
- * Display a post
+ * A user submitted text post
  */
 class Post extends Component {
     componentDidMount() {
