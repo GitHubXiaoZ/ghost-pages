ab92bb75b462796b747ce956e7647b63f2967990
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-11-30 02:42:37 -0500

    Update comment_feed.js

--- client/src/components/post/comment/comment_feed.js
+++ client/src/components/post/comment/comment_feed.js
@@ -5,7 +5,7 @@ import CommentItem from "./comment_item"
 
 /* Class: CommentFeed
  * Array of comments
- * Render a commentitem for each comment in comment_list
+ * Render a commentitem for each comment in comment_list array
  */
 class CommentFeed extends Component {
     render() {
