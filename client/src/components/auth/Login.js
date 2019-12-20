47ea15ac23d74b714d38a38924406ed696602f68
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-12-20 02:35:51 -0500

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -113,7 +113,7 @@ class Login extends Component {
 
 //login proptypes
 Login.propTypes = {
-    //{function} called to login exisiting user (email and password)
+    //{function} called to login existing user (email and password)
     loginUser: PropTypes.func.isRequired,
     //{object} auth component prop
     auth: PropTypes.object.isRequired,
