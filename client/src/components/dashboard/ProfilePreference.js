47f440e2feef2a1ec55932c3b6960084245a4127
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-12-22 01:25:05 -0500

    Update ProfilePreference.js

--- client/src/components/dashboard/ProfilePreference.js
+++ client/src/components/dashboard/ProfilePreference.js
@@ -3,7 +3,6 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 //display on user's dashboard 
-//edit user profile data
 const ProfilePreference = () => {
     return (
         <div>
