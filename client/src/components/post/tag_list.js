1d1fd11d621ae0475e6de8ed85e7f64bb037e339
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-12-26 02:24:18 -0500

    Update tag_list.js

--- client/src/components/post/tag_list.js
+++ client/src/components/post/tag_list.js
@@ -9,7 +9,7 @@ import { getTagList } from "../../actions/postActions"
 
 /* Class: TagList
  * Tag component
- * Display all tags found in user posts
+ * Display a list of all the tags found in posts
  */
 class TagList extends Component {
     componentDidMount() {
