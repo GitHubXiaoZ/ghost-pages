5f0d65b9fafbd3f8f833f58be246d57a38086500
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-01-03 02:51:59 -0500

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -8,7 +8,7 @@ import { loginUser } from "../../actions/authActions"
 
 /* Class: Login
  * Login form
- * Input email and password to login to user's account
+ * Input email and password to access user's account
  * Redirects to user's dashboard
  */
 class Login extends Component {
