7b25d602c8734f3708de1d91275b0108993412a8
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-01-07 02:10:33 -0500

    Update edit_comment.js

--- client/src/components/post/comment/edit_comment.js
+++ client/src/components/post/comment/edit_comment.js
@@ -7,7 +7,7 @@ import { editComment, getComment } from "../../../actions/postActions"
 
 /* Class: EditComment
  * Comment field form
- * Edit/update comment through text field form
+ * Edit/update comment through text field 
  */
 class EditComment extends Component {
     constructor(props) {
