2241d2c94ea4672066d2c31d227a7e1a20bd4b76
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-01-10 02:15:54 -0500

    Update new_profile.js

--- client/src/components/profile/new_profile.js
+++ client/src/components/profile/new_profile.js
@@ -7,7 +7,7 @@ import { newProfile } from "../../actions/profileActions"
 
 /* Class: NewProfile
  * Profile field form
- * Creates a new profile using user data
+ * Create a new profile using user data
  */
 class NewProfile extends Component {
     constructor(props) {
