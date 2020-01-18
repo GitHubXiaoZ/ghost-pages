603259e0d05f0e8aad465372c777b22c23b1b647
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-01-18 01:14:05 -0500

    Update novel.js

--- validate/novel.js
+++ validate/novel.js
@@ -2,7 +2,7 @@
 const Validator = require("validator")
 const isEmpty = require("is-empty")
 
-/*exports*/
+/*export*/
 module.exports = validateNovelInput = data => {
     let errors = {}
     const status = ["On Going", "Completed", "Hiatus"]
