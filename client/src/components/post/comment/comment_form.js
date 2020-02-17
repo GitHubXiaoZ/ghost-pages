685b6e5dcea77ac51def614f1a0542a6fb9bb18a
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-02-17 02:50:39 -0500

    Update comment_form.js

--- client/src/components/post/comment/comment_form.js
+++ client/src/components/post/comment/comment_form.js
@@ -7,7 +7,7 @@ import { addComment } from "../../../actions/postActions"
 
 /* Class: CommentForm
  * Comment field form
- * Add comment to post through text field  
+ * Add comment to post
  */
 class CommentForm extends Component {
     constructor(props) {
