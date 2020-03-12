ea6c747d1ea20444c2700b4977ab9e70bbdaec54
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-03-12 02:57:45 -0400

    Update edit_post.js

--- client/src/components/post/edit_post.js
+++ client/src/components/post/edit_post.js
@@ -8,7 +8,7 @@ import { editPost, getPost } from "../../actions/postActions"
 
 /* Class: EditPost
  * Post field form
- * Edit/update post content 
+ * Edit/update user's post content 
  */
 class EditPost extends Component {
     constructor(props) {
