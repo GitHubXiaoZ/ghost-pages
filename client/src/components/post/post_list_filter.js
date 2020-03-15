6a96cbb30baa0400620dbeb6b982e3796fdb1aaa
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-03-15 00:29:53 -0400

    Update post_list_filter.js

--- client/src/components/post/post_list_filter.js
+++ client/src/components/post/post_list_filter.js
@@ -12,7 +12,7 @@ import { getPostListByUser,
 
 /* Class: PostListFilter
  * Post list component
- * Display post feed containing a list of filtered posts
+ * A list of filtered posts
  * Posts are filtered through url params 
  */
 class PostListFilter extends Component {
