3b7f8140eb908a56abfb1f52a40a99085a6dad40
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-03-17 02:43:26 -0400

    Update Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-//profile model
+//profile 
 const ProfileSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
