7438d28086c9d185b704883d7ef9876db50977d7
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-03-22 02:39:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//database connection
+//connect to mongoDB
 connectToDB()
 
 //configurate passport using jwt token
