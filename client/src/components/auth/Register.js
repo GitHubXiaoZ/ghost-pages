95edb43a153c41a28125e639df4389d1583598ae
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-03-25 01:24:08 -0400

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,7 +10,7 @@ import { registerUser } from "../../actions/authActions"
 /* Class: Register
  * Register form
  * Create a new user account
- * Redirect to dashboard after registration
+ * Redirect to user's dashboard after registration
  */
 class Register extends Component {
     constructor() {
