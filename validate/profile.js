cf9ad1aec1fb993d625d3306dff9c8707b198d73
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-04-07 01:42:49 -0400

    Update profile.js

--- validate/profile.js
+++ validate/profile.js
@@ -9,10 +9,7 @@ module.exports = validateProfileInput = data => {
     //alphanumeric characters
     const regex = /^[a-zA-z0-9-]+$/g
 
-    /*set
-     *handle
-     *to empty string if no input is given
-     */
+    //set handle to empty string if no input is given
     data.handle = !isEmpty(data.handle) ? data.handle : ""
 
     const valid_profile = data.handle.match(regex)
