c0acf52b771f1accf7f76759063a9e558e56acfd
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-06-13 01:39:41 -0400

    Update comment_item.js

--- client/src/components/post/comment/comment_item.js
+++ client/src/components/post/comment/comment_item.js
@@ -13,6 +13,7 @@ import { deleteComment } from "../../../actions/postActions"
  * delete function
  */
 class CommentItem extends Component {
+    //delete comment
     onDelete = (post_id, comment_id) => {
         this.props.deleteComment(post_id, comment_id)
     }
