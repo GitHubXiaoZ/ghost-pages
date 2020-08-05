aa79b18cb7ec45333bd906cf7e37fa4f9f59dc2e
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-08-05 00:36:40 -0400

    Update register.js

--- validate/register.js
+++ validate/register.js
@@ -24,6 +24,7 @@ module.exports = validateRegisterInput = data => {
     const valid_name = data.name.match(regex)
 
     //validators
+    //name
     if (Validator.isEmpty(data.name)) {
         errors.name = "Name required!"
     } else if (!valid_name) {
