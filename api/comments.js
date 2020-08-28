17d406ec24d8991631e28b57bce5de8f173d0a98
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-08-28 00:15:35 -0400

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -43,6 +43,7 @@ router.delete("/:id",
         .then(profile => {
             Comment.findById(req.params.id)
                 .then(comment => {
+                    //if comment was created by user
                     if (comment.user.toString() !== req.user.id) {
                         return res.status(401).json({ nopermission: "Not allowed to delete this comment!" })
                     }
