52435a3cb4208a7e7f6e82900ec12b556338998b
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2020-09-03 00:36:13 -0400

    Update setAuthToken.js

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,14 +3,14 @@ import axios from "axios"
 
 /**function setAuthToken
  * @param token (jwt token)
- * authenticates user using jwt token
+ * authenticate user
  */
 const setAuthToken = token => {
     if (token) {
-        //sets an authorize token to every request
+        //set an authorize token to every request
         axios.defaults.headers.common["Authorization"] = token
     } else {
-        //deletes auth header
+        //delete auth header
         delete axios.defaults.headers.common["Authorization"] 
     }
 }
